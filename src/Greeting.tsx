03c0489db4c9e106be15e2c297255558149475b0
Fernando Martínez <fernando_m1911a1@hotmail.com>
2024-08-08 15:36:20 -0600

    Creates greeting component

---
+++ src/Greeting.tsx
@@ -0,0 +1,10 @@
+const Greeting: React.FC = () => {
+  return (
+    <div className="greeting">
+      <h1>Fernando's Calculator</h1>
+      <h4>Made with React</h4>
+    </div>
+  )
+}
+
+export default Greeting
